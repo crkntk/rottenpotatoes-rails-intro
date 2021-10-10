18a916e26cceae1910c6b18f8190f61515983c0c
Carlos Reyes <creyes37@berkeley.edu>
2021-10-10 20:04:24 +0000

    dayum

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,15 @@
 class Movie < ActiveRecord::Base
-	ratings_to_show =[]
 	
 	
 	
-	
-	def all_ratings
 		@all_ratings = ['G', 'PG', 'PG-13', 'R']
-	end
+	
 	def with_ratings(ratings)
 		if(ratings!=nil)
 		movies.where('rating = ?', ratings)
 		else
 			movies.all
 		end
+		
 	end
 end
